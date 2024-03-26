7b88d9cf6cb6aa51fd60912d462c9f255cdb04f9
IIROFESSOR-X <IIROFESSOR@gmail.com>
2024-03-26 22:46:06 +0200

    fix

--- src/components/ImageGallery/ImageCard/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard/ImageCard.jsx
@@ -1,10 +1,18 @@
 import css from './ImageCard.module.css';
-const ImageCard = ({ url, id, alt }) => {
+
+const ImageCard = ({ url, id, alt, onClick }) => {
   const src = url + `&h=400`;
+
+  const handleClick = () => {
+    onClick(id);
+  };
+
   return (
     <div className={css.container}>
-      <img src={src} data-id={id} alt={alt} />
+      {/* Добавляем обработчик onClick */}
+      <img src={src} alt={alt} onClick={handleClick} />
     </div>
   );
 };
+
 export default ImageCard;
