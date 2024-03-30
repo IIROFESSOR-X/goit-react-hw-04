70b3b88396ecd54cffacebf481a6fbf779264f7b
IIROFESSOR-X <IIROFESSOR@gmail.com>
2024-03-30 15:32:28 +0200

    check

--- src/components/ImageGallery/ImageCard/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard/ImageCard.jsx
@@ -1,15 +1,16 @@
 import css from './ImageCard.module.css';
 
 const ImageCard = ({ url, id, alt, onClick }) => {
-  const src = url + `&h=400`;
-
   const handleClick = () => {
-    onClick(id);
+    if (onClick) {
+      onClick(id);
+    }
   };
+
+  const src = url + `&h=400`;
 
   return (
     <div className={css.container}>
-      {/* Добавляем обработчик onClick */}
       <img src={src} alt={alt} onClick={handleClick} />
     </div>
   );
